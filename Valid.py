96a8279f871fba3f416d37e38dca0823c1e3617a
Trenton Smith <Trentonsmith608@gmail.com>
2025-02-24 21:47:06 -0500

    Add files via upload

--- Valid.py
+++ Valid.py
@@ -12,13 +12,19 @@ class Valid:
   """
   @staticmethod
   def zip(input):
+    try:
+      int(input)
+      if re.match(r"^\d{5}$", input):
+        return True
+    except ValueError as e:
+      print("not a valid zip. enter 5 digits e.g.'12345'")
     #length?
 
     #digits?
 
     #real ZIP?
     
-    return True
+    return False
   
   """
   Validate a ZIP code. return true if the input is a valid ZIP, 
@@ -31,6 +37,14 @@ class Valid:
   """
   @staticmethod
   def phone(input):
+    try:
+      if re.match(r"^\+?\d{1,3} \(\d{3}\) \d{3}-\d{4}$", input):
+        return True
+    except ValueError:
+      print(
+        "Not a valid phone number. Enter in the format '(656) 777-7777' or with a country code like '+1 (656) 777-7777'.")
+      return False
+
     #length?
 
     #digits?
@@ -39,4 +53,4 @@ class Valid:
 
     #real number?
     
-    return True
+    return False
